394aae67d26f54a367932fb35f26f308fdd7bcd0
shehzar1 <muhammad.shehzar@square63.org>
2021-10-27 15:05:49 +0500

    Added CRUD operations for Ads

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,13 +1,48 @@
 class AdsController < ApplicationController
+  before_action :current_ad, only: [:show, :edit, :update, :destroy]
+
   def index
+    @ads = Ad.all
   end
 
   def show
+
   end
 
   def new
+    @ad = Ad.new
+  end
+
+  def create
+    ad = Ad.create(Ad_params)
+
+    redirect_to Ad_path(ad)
   end
 
   def edit
+
+  end
+
+  def update
+    @ad.update(Ad_params)
+
+    redirect_to Ad_path(@ad)
+  end
+
+  def destroy
+    @ad.destroy
+    redirect_to Ads_path
+  end
+
+  private
+
+  def current_ad
+    @ad = Ad.find(params[:id])
+  end
+
+  private
+
+  def ad_params
+    params.require(:ad).permit(:city, :milage, :car_make,:price,:engine_type,:transmission_type,:engine_capacity ,:color, :assembly_type, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
+
   resources :ads
-  get 'ads/index'
-  get 'ads/show'
-  get 'ads/new'
-  get 'ads/edit'
+
   devise_for :users
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
