473f350d398fe1084234b5fe783a09be3c1bec62
shehzar1 <muhammad.shehzar@square63.org>
2021-10-27 12:38:05 +0500

    Added MVC for Ads

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_093420) do
+ActiveRecord::Schema.define(version: 2021_10_27_073413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ads", force: :cascade do |t|
+    t.string "city"
+    t.integer "milage"
+    t.string "car_make"
+    t.integer "price"
+    t.string "engine_type"
+    t.string "transmission_type"
+    t.integer "engine_capacity"
+    t.string "color"
+    t.string "assembly_type"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
