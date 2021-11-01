5a0ed66f51bf87b1669fb49c2542546e2ad519cf
shehzar1 <muhammad.shehzar@square63.org>
2021-11-01 16:20:31 +0500

    Added Proper Indentation

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -5,7 +5,7 @@ class AdsController < ApplicationController
     @ads = Ad.all
   end
 
-  def show;end
+  def show; end
 
   def new
     @ad = Ad.new
@@ -16,7 +16,7 @@ class AdsController < ApplicationController
     redirect_to ad_path(ad)
   end
 
-  def edit;end
+  def edit; end
 
   def update
     @ad.update(ad_params)
