533a2dae4b7b11f51e684ed7e45b319e962c120a
shehzar1 <muhammad.shehzar@square63.org>
2021-11-09 10:50:46 +0500

    Payment integrated via Stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'charges/new'
+  get 'charges/create'
   root to: "home#index"
   devise_for :users
   resources :ads do
@@ -7,5 +9,6 @@ Rails.application.routes.draw do
     end
   end
   resources :ad_steps
+  resources :charges, only: [:new, :create]
   get 'home/index'
 end
