cc1fc0777a2609c1c4b9729646c57106803502a6
shehzar1 <muhammad.shehzar@square63.org>
2021-11-10 15:24:44 +0500

    Configuration for js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
 import 'bootstrap';
-Rails.start();
-Turbolinks.start();
-ActiveStorage.start();
-require ("jquery3");
-require ("popper");
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("custom.js");
+import $ from 'jquery';
+global.$ = jQuery;
+
+console.log('Hello from application.js')
