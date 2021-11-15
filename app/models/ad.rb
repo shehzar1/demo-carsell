e148bd3de2a682e33d0cc8bee18e1cf1f508247b
shehzar1 <muhammad.shehzar@square63.org>
2021-11-15 09:26:26 +0500

    Added validation for imgs

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -16,6 +16,14 @@ class Ad < ApplicationRecord
   PER_PAGE_COUNT = 4.freeze
   PHONE_REGEX = /^((\+92))-{0,1}\d{3}-{0,1}\d{7}$/.freeze
 
+  validate :validate_images
   validates :primary_contact, format: {with: /^[0-9]{11}$/, message: "format should be +92-3XX-XXXXXXX", multiline: true}, allow_blank: true
   validates :secondary_contact, format: {with: PHONE_REGEX, message: "format should be +92-3XX-XXXXXXX", multiline: true}, allow_blank: true
+
+  private
+
+  def validate_images
+    return if images.count < 6
+    errors.add(:images, 'You cannot upload more than 5 for an ad.')
+  end
 end
