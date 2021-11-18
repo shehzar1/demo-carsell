ae7835e827d52eac258c3d659d6249a7738f8abc
shehzar1 <muhammad.shehzar@square63.org>
2021-11-18 16:12:59 +0500

    Added Stripe API

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'charges/new'
+  get 'charges/create'
   root to: "home#index"
   devise_for :users
+
   resources :ads do
     member do
       get :favorites
@@ -14,5 +17,7 @@ Rails.application.routes.draw do
   end
 
   resources :ad_steps
+
+  resources :charges, only: [:new, :create]
   get 'home/index'
 end
