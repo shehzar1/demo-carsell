aa4e5199e2ff53ff59ddfa192751aac84bfbb7e4
shehzar1 <muhammad.shehzar@square63.org>
2021-11-23 17:20:20 +0500

    Temp

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -24,11 +24,12 @@ class Ad < ApplicationRecord
 
   def self.search(query_hash)
     scope = Ad.all
-    if query_hash.present?
-      query_hash.each do |key, value|
-        scope = scope.search_ads(key, value) if (value.present?)
-      end
-    end
+    scope = filter if query_hash.present?
+
     scope
+  end
+
+  def self.filter
+    query_hash.each { |key, value| self.search_ads(key, value) if (value.present?) }
   end
 end
