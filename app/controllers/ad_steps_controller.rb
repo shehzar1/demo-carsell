f293bfcb719f955db1f2836cddf4c70ed167a103
shehzar1 <muhammad.shehzar@square63.org>
2021-11-24 15:20:48 +0500

    updated adstep controller 2

--- app/controllers/ad_steps_controller.rb
+++ app/controllers/ad_steps_controller.rb
@@ -13,7 +13,7 @@ class AdStepsController < ApplicationController
     if params.dig(:ad, :images).present?
       img_count = @ad.images.count
       @ad.images.attach(ad_images_params[:images])
-      redirect_to ad_steps_url(:image_step, ad_id: @ad.id), alert: "No new image attached: Please try again." and return if img_count >= @ad.images.all.count
+      redirect_to ad_steps_url(:image_step, ad_id: @ad.id), alert: "No new image attached: Please try again." and return if img_count >= @ad.images.count
     end
     render_wizard(@ad, {}, ad_id: @ad)
   end
